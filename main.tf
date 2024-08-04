c15dc4f9f9c4c857ca37d9992cc93f75981738b3
unknown <kushalsingwani12@gmail.com>
2024-08-04 11:14:16 +0530

    update5

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
   # apply s3 backend on aws account 
   backend "s3" {
-    bucket = "terraform-s3-class-630"
+    bucket = "terraform-aws-s3-30"
     key    = "statecollection"
     region = "ap-south-1"
   }
@@ -26,11 +26,4 @@ module "jenkins-class-630-dev" {
   vpc_dev_cidr = "10.10.0.0/16"
   vpc_dev_subnet_cidr = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24", "10.10.4.0/24"]
   vpc_dev_subnet_az = ["ap-south-1a", "ap-south-1b"]
-}
-
-module "jenkins-class-630-prod" {
-  source = "./modules/prod"
-  vpc_prod_cidr = "10.20.0.0/16"
-  vpc_prod_subnet_cidr = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24", "10.20.4.0/24"]
-  vpc_prod_subnet_az = ["ap-south-1a", "ap-south-1b"]
 }
